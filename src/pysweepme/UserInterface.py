debac22098c53febd93a11724dc46bb3b02b82c5
Axel Fischer <axel.fischer@sweep-me.net>
2022-07-17 22:24:10 +0200

    new submodule "UserInterface" with get_input method to get user feedback

---
+++ src/pysweepme/UserInterface.py
@@ -0,0 +1,37 @@
+# The MIT License
+
+# Copyright (c) 2021-2022 SweepMe! GmbH
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+def get_input(msg: str) -> str:
+    """
+    This function can be used by drivers or CustomFunction scripts to ask the user for input. If used with SweepMe!,
+    this function will be overloaded by SweepMe! to create a graphical user interface.
+    
+    Args:
+        msg: String of the message that is displayed to the user
+
+    Returns:
+        str: response message
+
+    """
+
+    return input(msg)
