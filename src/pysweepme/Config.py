b7b9f848da610bd613fd586293f13fab4bf371ed
Felix Kaschura <felix.kaschura@sweep-me.net>
2023-12-12 10:35:45 +0100

    Add option to define a custom class for opening a file compatible to Path.open() on a global and per-use basis

--- src/pysweepme/Config.py
+++ src/pysweepme/Config.py
@@ -20,39 +20,89 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from __future__ import annotations
+
 import os
 from configparser import ConfigParser
+from pathlib import Path
+from types import TracebackType
+from typing import IO, Any, Callable, Protocol, Union, cast
 
 from pysweepme.ErrorMessage import error
-from pysweepme.FolderManager import getFoMa
 
 
-def is_nonprimary_instance() -> bool:
-    return bool(getFoMa().get_instance_id())
+class FileIOContextProtocol(Protocol):
+    def __enter__(self) -> IO[Any]:
+        """Function to return a file descriptor."""
+
+    def __exit__(
+        self,
+        exc_type: type[Exception] | None,
+        exc_value: Exception | None,
+        traceback: TracebackType | None,
+    ) -> bool | None:
+        """Function to close context manager."""
 
 
-def get_write_mode() -> str:
-    """The write mode overwrite 'w' for file operations, except when this pysweepme instance is not the only / first
-    instance. In that case it returns 'r' so that no conflicting write operations can occur.
+class FileIOProtocolWithoutModifiedCheck(Protocol):
+    def open(  # noqa: A003, PLR0913
+        self,
+        mode: str = "r",
+        buffering: int = -1,
+        encoding: str | None = None,
+        errors: str | None = None,
+        newline: str | None = None,
+    ) -> FileIOContextProtocol:
+        """Function to open a file compatible with pathlib.Path when used in a context manager."""
 
-    Returns:
-        The write mode for file operations.
-    """
-    if getFoMa().get_instance_id():
-        return "r"
-    else:
-        return "w"
+
+class FileIOProtocolWithModifiedCheck(FileIOProtocolWithoutModifiedCheck):
+    def set_full_read(self) -> None:
+        """Function that shall be called when a file is read completely.
+
+        If a file was modified by an external program, the user does not need to decide whether to overwrite the file
+        or not, if the respective file was only read after the external modification.
+        """
+
+
+FileIOProtocol = Union[FileIOProtocolWithoutModifiedCheck, FileIOProtocolWithModifiedCheck]
+
+
+class DefaultFileIO:
+    custom_default_file_io: tuple[Callable[[Path | str], FileIOProtocol]] | None = None
+
+    @classmethod
+    def register_custom_default(cls, function: Callable[[Path | str], FileIOProtocol]) -> None:
+        # use a tuple to prevent python from considering it a bound method
+        cls.custom_default_file_io = (function,)
+
+    @staticmethod
+    def pysweepme_default_fileio(file: Path | str) -> FileIOProtocol:
+        # Path has more sophisticated signatures than the FileIOProtocol, so we need to cast the type
+        return cast(FileIOProtocol, Path(file))
+
+    @staticmethod
+    def default_fileio(file: Path | str) -> FileIOProtocol:
+        if DefaultFileIO.custom_default_file_io:
+            return DefaultFileIO.custom_default_file_io[0](file)
+        return DefaultFileIO.pysweepme_default_fileio(file)
 
 
 class Config(ConfigParser):
     """Convenience wrapper around ConfigParser to quickly access config files."""
 
-    def __init__(self, file_name=None) -> None:
+    def __init__(
+        self,
+        file_name: Path | str,
+        custom_reader_writer: Callable[[Path | str], FileIOProtocol] = DefaultFileIO.default_fileio,
+    ) -> None:
         super().__init__()
 
         self.optionxform = str  # type: ignore
 
         self.file_name = file_name
+
+        self.reader_writer = custom_reader_writer(file_name)
 
     def setFileName(self, file_name):
         """Deprecated."""
@@ -77,20 +127,20 @@ class Config(ConfigParser):
         """Deprecated."""
         return self.load_file()
 
-    def load_file(self):
+    def load_file(self) -> bool:
         try:
             if self.is_file():
-                if os.path.exists(self.file_name):
-                    with open(self.file_name, "r", encoding="utf-8") as cf:
-                        self.read_file(cf)
-                return True
+                with self.reader_writer.open("r", encoding="utf-8") as cf:
+                    self.read_file(cf)
+                    if hasattr(self.reader_writer, "set_full_read"):
+                        self.reader_writer.set_full_read()
             else:
                 self.read_string(self.file_name)
-                return True
         except:
             error()
+            return False
 
-        return False
+        return True
 
     def makeConfigFile(self):
         """Deprecated."""
@@ -99,14 +149,10 @@ class Config(ConfigParser):
     def create_file(self):
         try:
             if not self.is_file():
-                if is_nonprimary_instance():
-                    msg = "Config File cannot be created in multi-instance mode. Please close all SweepMe! instances and start a single SweepMe! instance first."
-                    raise Exception(
-                        msg,
-                    )
                 if not os.path.exists(os.path.dirname(self.file_name)):
                     os.mkdir(os.path.dirname(self.file_name))
-                with open(self.file_name, get_write_mode(), encoding="utf-8") as cf:
+
+                with self.reader_writer.open("w", encoding="utf-8") as cf:
                     self.write(cf)
 
                 return True
@@ -119,66 +165,46 @@ class Config(ConfigParser):
         """Deprecated."""
         return self.set_section(section)
 
-    def set_section(self, section):
+    def set_section(self, section: str) -> bool:
         try:
             if self.load_file():
-                if is_nonprimary_instance():
-                    msg = "Cannot save config in multi-instance mode. Please change the configuration in the primary SweepMe! instance."
-                    raise Exception(
-                        msg,
-                    )
-                with open(self.file_name, get_write_mode(), encoding="utf-8") as cf:
-                    if not self.has_section(section):
-                        self.add_section(section)
+                if not self.has_section(section):
+                    self.add_section(section)
+                with self.reader_writer.open("w", encoding="utf-8") as cf:
                     self.write(cf)
-            return True
-
         except:
             error()
+            return False
 
-        return False
+        return True
 
     def setConfigOption(self, section, option, value):
         """Deprecated."""
         return self.set_option(section, option, value)
 
-    def set_option(self, section, option, value):
+    def set_option(self, section: str, option: str, value: str) -> bool:
         try:
-            if is_nonprimary_instance():
-                msg = "Cannot save config in multi-instance mode. Please change the configuration in the primary SweepMe! instance."
-                raise Exception(
-                    msg,
-                )
-
             self.set_section(section)
-
             self.set(section, option, value)
-
-            with open(self.file_name, get_write_mode(), encoding="utf-8") as cf:
+            with self.reader_writer.open("w", encoding="utf-8") as cf:
                 self.write(cf)
-            return True
+        except:
+            error()
+            return False
+
+        return True
+
+    def removeConfigOption(self, section: str, option: str) -> bool:
+        try:
+            if self.load_file() and self.has_section(section) and self.has_option(section, option):
+                self.remove_option(section, option)
+                with self.reader_writer.open("w", encoding="utf-8") as cf:
+                    self.write(cf)
+                return True
         except:
             error()
 
         return False
-
-    def removeConfigOption(self, section, option):
-        try:
-            if self.load_file() and self.has_section(section) and self.has_option(section, option):
-                if is_nonprimary_instance():
-                    msg = "Cannot save config in multi-instance mode. Please change the configuration in the primary SweepMe! instance."
-                    raise Exception(
-                        msg,
-                    )
-                self.remove_option(section, option)
-
-                with open(self.file_name, get_write_mode(), encoding="utf-8") as cf:
-                    self.write(cf)
-
-                return True
-            return False
-        except:
-            error()
 
     def getConfigSections(self):
         """Deprecated."""
