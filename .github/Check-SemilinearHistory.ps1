bf9d784eeafcf79559d02f6df1935267e71251c9
Felix Kaschura <felix.kaschura@sweep-me.net>
2026-01-26 10:25:06 +0100

    Remove workflows for 1.5.6 (in preparation for 1.5.8)

--- .github/Check-SemilinearHistory.ps1
+++ .github/Check-SemilinearHistory.ps1
@@ -8,9 +8,10 @@ Param(
 $ErrorActionPreference = 'Stop'
 
 $versionBranches = @(
-  "main",
-  "1.5.6",
-  "v1.5.5.x"
+    "main",
+    "1.5.7",
+    "1.5.6",
+    "v1.5.5.x"
 )
 
 git fetch origin $targetBranch
@@ -21,15 +22,13 @@ $commitSource = git rev-parse "origin/$sourceBranch"
 # verify that source branch originates from the latest commit of the target branch
 # (i.e. a fast-forward merge could be performed)
 git merge-base --is-ancestor $commitTarget $commitSource
-if ($LASTEXITCODE -ne "0")
-{
-   throw "Merge would create a non-semilinear history."
+if ($LASTEXITCODE -ne "0") {
+    throw "Merge would create a non-semilinear history."
 }
 
 # the target branch should only contain simple commits and no merges
 $numberOfMergeCommits = git rev-list --min-parents=2 --count "${commitTarget}..${commitSource}"
-if ($numberOfMergeCommits -eq "0")
-{
+if ($numberOfMergeCommits -eq "0") {
     Return
 }
 
@@ -37,13 +36,11 @@ $commonError = "Source Branch contains non-linear history. This is only acceptab
 
 # find the branch that corresponds to the version before
 $index = [array]::IndexOf($versionBranches, $targetBranch)
-if ($index -eq "-1")
-{
+if ($index -eq "-1") {
     throw "$commonError the Target Branch does not correspond to a release version."
 }
 $precedingVersionBranch = $versionBranches[$index + 1]
-if ($precedingVersionBranch -eq $null)
-{
+if ($precedingVersionBranch -eq $null) {
     throw "$commonError there is no preceding version that is allowed to be merged into the Target Branch."
 }
 
@@ -55,13 +52,11 @@ $currentMergeBase = git merge-base "origin/$precedingVersionBranch" "origin/$tar
 # This must be a true descendent of the current merge base, otherwise the merge is not merging the changes from the old version into the new version
 $newMergeBase = git merge-base "origin/$precedingVersionBranch" "origin/$sourceBranch"
 
-if ($currentMergeBase -eq $newMergeBase)
-{
+if ($currentMergeBase -eq $newMergeBase) {
     throw "$commonError the merge into the $targetBranch branch does not include commits from the $precedingVersionBranch branch (merge bases are equal)."
 }
 
 git merge-base --is-ancestor $currentMergeBase $newMergeBase
-if ($LASTEXITCODE -ne "0")
-{
-   throw "$commonError the merge into the $targetBranch branch does not include commits from the $precedingVersionBranch branch (merge bases are in the wrong order)."
+if ($LASTEXITCODE -ne "0") {
+    throw "$commonError the merge into the $targetBranch branch does not include commits from the $precedingVersionBranch branch (merge bases are in the wrong order)."
 }
